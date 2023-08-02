a8f0a64817ac4d0e3080791828fbb6b44125931f
Julian.HumerHager <julian.humerhager@physik.uni-muenchen.de>
2023-08-02 17:30:28 +0200

    inserted sin and in cloud oscillation, fixed timescale in plots

--- parcel.py
+++ parcel.py
@@ -17,10 +17,38 @@ def parcel_rho(P_parcel, T_parcel):
     return(rho_parcel, V_parcel, air_mass_parcel) 
 
 
-def ascend_parcel(z_parcel, T_parcel,P_parcel, dz):
-    z_parcel   = z_parcel + dz
+def ascend_parcel(z_parcel, T_parcel,P_parcel,w_parcel,dt, time, time_half_wave_parcel=1200.0, ascending_mode='linear', t_start_oscillation=800, max_z=1400):
     
-    T_parcel   = T_parcel - dz * g / cp
+    if ascending_mode=='linear':
+        if z_parcel < max_z: 
+            dz = w_parcel * dt
+            z_parcel   = z_parcel + dz
+            T_parcel   = T_parcel - dz * g / cp
+        
+    elif ascending_mode=='sine': 
+        w_oscillate = w_parcel * np.pi / 2.0 * np.sin(np.pi * time / time_half_wave_parcel)
+        dz = w_oscillate  * dt
+        z_parcel = z_parcel + dz
+        if w_parcel > 0:
+            T_parcel = T_parcel - dz * g / cp
+        else:
+            T_parcel = T_parcel + dz * g / cp
+            
+    elif ascending_mode=='in_cloud_oscillation':
+        if time < t_start_oscillation:
+            dz = w_parcel * dt
+            z_parcel   = z_parcel + dz
+            T_parcel   = T_parcel - dz * g / cp
+        else:
+            w_oscillate = w_parcel * np.pi / 2.0 * np.cos(np.pi * (time-t_start_oscillation) / time_half_wave_parcel)
+            dz = w_oscillate  * dt
+            z_parcel = z_parcel + dz
+            if w_parcel > 0:
+                T_parcel = T_parcel - dz * g / cp
+            else:
+                T_parcel = T_parcel + dz * g / cp
+    #w_parcel = w_mean_parcel * pi / 2.0 * SIN( pi * time / time_half_wave_parcel )
+    
     rho_parcel, V_parcel, air_mass_parcel =  parcel_rho(P_parcel, T_parcel)
     
     return z_parcel, T_parcel, rho_parcel, V_parcel, air_mass_parcel 
